bd089ce6b964b6524ac8f2fedba0b41b175dc99d
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-23 18:26:20 -0300

    feat: add validator interceptor in signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,25 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('hi there')
+router.post('/api/users/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be provided'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters')
+], (req: Request, res: Response) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array())
+  }
+  // const { email, password } = req.body
+  console.log('creating a user...')
+  res.send({})
 })
 
 export { router as signupRouter }
