6034ff3ee1c45637441f29abecb359b0e3f8536c
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-24 17:56:42 -0300

    feat: add interceptor service to singup route

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { Password } from '../services/password'
 
 // An interface that describes the properties required to create new user
 interface UserAttrs {
@@ -27,6 +28,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+})
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => {
