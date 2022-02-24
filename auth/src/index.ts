a66c63d2ef73d2dc3056f52f0182aa1934661c6c
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-24 15:08:35 -0300

    feat: add mongoDb connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 import { currentuserRouter, signoutRouter, signinRouter, signupRouter } from './routes/index'
 import { errorHandler } from './middlewares/error-handler'
 
@@ -13,6 +14,17 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(errorHandler)
 
+const start = async (): Promise<void> => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('mongodb connected')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 app.listen(3000, () => {
   console.log('listening on http://localhost:3000!!')
 })
+
+void start()
