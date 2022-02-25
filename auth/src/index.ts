63f768b7781fb199e57b1b8922b868def68db2ed
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-25 14:32:40 -0300

    fix: reorder express cookieSession middleware and add env variables into containers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,19 +8,18 @@ import cookieSession from 'cookie-session'
 
 const app = express()
 app.set('trust proxy', true)
-app.use(json())
-
-app.use(currentuserRouter)
-app.use(signupRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(errorHandler)
 app.use(
   cookieSession({
     signed: false,
     secure: true
   })
 )
+app.use(json())
+app.use(currentuserRouter)
+app.use(signupRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(errorHandler)
 
 const start = async (): Promise<void> => {
   try {
