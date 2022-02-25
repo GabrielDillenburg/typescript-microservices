a159db8fd6a7b8cae2d6020cfa531dacaa2e79a8
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-25 18:43:25 -0300

    feat: add current user middleware to validate requests

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+interface UserPayload {
+  id: string
+  email: string
+}
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  if (!req.session?.jwt) {
+    return next()
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY) as UserPayload
+    req.currentUser = payload
+  } catch (err) {
+
+  }
+
+  next()
+}
