9cae2b265e7cc058cef3886ba94e4ad674dd256f
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-26 11:24:58 -0300

    test: ensure that fails when an incorrect password is supplied

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -11,3 +11,21 @@ it('fails when a email does not exist is supplied', async () => {
     })
     .expect(400)
 })
+
+it('fails when an incorrect password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@example.com',
+      password: 'password'
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@example.com',
+      password: 'other_password'
+    })
+    .expect(400)
+})
