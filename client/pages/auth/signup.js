2d6e6c0006239d48702bafc75e607b431ff41249
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-26 17:05:53 -0300

    feat: add Signup component

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+export default () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(email, password)
+  }
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Signup</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input 
+          value={email} 
+          onChange={e => setEmail(e.target.value)} 
+          className="form-control"/>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input 
+          value={password} 
+          onChange={e => setPassword(e.target.value)} 
+          className="form-control"/>
+      </div>
+      <button className="btn btn-primary">Signup</button>
+    </form>
+  )
+}
