96a69a40c7f30899edbc1ab99e69cd42a923c317
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-26 12:42:57 -0300

    refactor: add signin helper

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,22 +1,16 @@
 import request from 'supertest'
 import { app } from '../../app'
+import { signin } from '../../test/setup'
+
 process.env.JWT_KEY = 'any_key'
 
 it('response with details about the current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@example.com',
-      password: 'password'
-    })
-    .expect(201)
-
-  const cookie = authResponse.get('Set-Cookie')
+  const cookie = await signin()
 
   const response = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
     .expect(200)
-  expect(response.body.currentUser.email).toEqual('test@example.com')
+  expect(response.body.currentUser.email).toEqual('test@test.com')
 })
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,7 @@
+import request from 'supertest'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
-// import { app } from '../app'
+import { app } from '../app'
 
 let mongo: any
 
@@ -23,3 +24,17 @@ afterAll(async () => {
   await mongo.stop()
   await mongoose.connection.close()
 })
+
+export const signin = async (): Promise<string[]> => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201)
+
+  const cookie = response.get('Set-Cookie')
+
+  return cookie
+}
