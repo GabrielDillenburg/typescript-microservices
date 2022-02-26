0f3a39a7b0748e31b7ceafef2a60420e5f9d4a3a
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-26 09:09:34 -0300

    refactor: add app.ts file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,32 +1,5 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
 import mongoose from 'mongoose'
-import { currentuserRouter, signoutRouter, signinRouter, signupRouter } from './routes/index'
-import { errorHandler } from './middlewares/error-handler'
-import cookieSession from 'cookie-session'
-import { NotFoundError } from './errors/handlers/not-found-error'
-
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-)
-app.use(currentuserRouter)
-app.use(signupRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-app.all('*', async (_req, _res): Promise<void> => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
+import { app } from './app'
 
 const start = async (): Promise<void> => {
   if (!process.env.JWT_KEY) {
