728077bd4800043ada15571050d45b5fa64f3fb3
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-26 12:45:32 -0300

    test: ensure currentUser response equal to null if not authenticated

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -14,3 +14,11 @@ it('response with details about the current user', async () => {
     .expect(200)
   expect(response.body.currentUser.email).toEqual('test@test.com')
 })
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200)
+  expect(response.body.currentUser).toEqual(null)
+})
