a749b315ad602cd65fc6b9b7352a64daa0a3fc3c
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-26 10:54:11 -0300

    test: ensure singup route returns status 400 with an invalid email or password

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,25 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201)
 })
+
+it('returns a 400 with an invalid email', async () => {
+  process.env.JWT_KEY = 'any_key'
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testexample.com',
+      password: 'password'
+    })
+    .expect(400)
+})
+
+it('returns a 400 with an invalid password', async () => {
+  process.env.JWT_KEY = 'any_key'
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@example.com',
+      password: 'pas'
+    })
+    .expect(400)
+})
