44c0559d2935f8e66402ce0d19fb158c01493362
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-03-02 14:09:30 -0300

    feat: ensure getInitialProps calls cross namespace communication

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
  - Custom implementation of an event bus (NATS Streaming Server)  
  - Structured responses from different API's
  - Best practices in communication between different services
- - Docker and Kubernetes deployments
- - JWT-based authentication :)
- - Unit/Integration tests using JEST
+ - Docker and Kubernetes deployments. Ingress Nginx service to routing!
+ - JWT-based authentication
+ - Unit/Integration tests using JEST :)
  - CI/CD
  - Third payment gateway API implementation (Stripe)
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,22 @@
+import axios from 'axios'
 
-const App = () => {
-  return <h1>Ola mundo</h1>;
-};
+const LandingPage = ({ currentuser }) => {
+  return <h1>Landing Page</h1>;
+}
 
-export default App
+LandingPage.getInitialProps = async ( { req }) => {
+
+  if ( typeof window === 'undefined') {
+    // cross namespace communication kubernetes
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', { 
+       headers: req.headers
+    })
+    return data
+  } else {
+    const { data } = await axios.get('/api/users/currentuser')
+    return data
+  }
+}
+
+export default LandingPage
