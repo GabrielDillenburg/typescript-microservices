fa49936deaf780ef6db0f7d7d50c3dfda05625ec
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-03-04 17:25:03 -0300

    test: ensure saving ticket on database

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { app } from '../../app'
+import { Ticket } from '../../models/tickets'
 import { signin } from '../../test/setup'
 
 it('has a router handler listening to /api/tickets for post requests', async () => {
@@ -24,7 +25,6 @@ it('returns an status other than 401 if user is singed in', async () => {
     .post('/api/tickets')
     .set('Cookie', cookie)
     .send({})
-  console.log(response.status)
   expect(response.status).not.toEqual(401)
 })
 
@@ -55,5 +55,20 @@ it('returns an error if an invelid price is provided', async () => {
 })
 
 it('creates a ticket with valid inputs', async () => {
+  const cookie = signin()
 
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'Some Title',
+      price: 10
+    })
+    .expect(201)
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
 })
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,14 +1,20 @@
 import express, { Request, Response } from 'express'
 import { requireAuth, validateRequest } from '@gdmtech/common'
 import { body } from 'express-validator'
+import { Ticket } from '../models/tickets'
 
 const router = express.Router()
 
 router.post('/api/tickets', requireAuth, [
   body('title').not().isEmpty().withMessage('Title is required'),
-  body('Price').isFloat({ gt: 0 }).isEmpty().withMessage('Price is required and greater than zero')
-], validateRequest, (req: Request, res: Response) => {
-  res.sendStatus(200)
+  body('price').isFloat({ gt: 0 }).withMessage('Price is required')
+], validateRequest, async (req: Request, res: Response) => {
+  const { title, price } = req.body
+  const ticket = Ticket.build({ title, price, userId: req.currentUser.id })
+
+  await ticket.save()
+
+  res.status(201).send(ticket)
 })
 
 export { router as createTicketRouter }
