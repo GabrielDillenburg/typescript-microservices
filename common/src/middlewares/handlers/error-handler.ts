11156b741825e5b885bbdb7734c6ed2d85b68cc6
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-03-06 10:47:48 -0300

    feat: add error log in error-handler middleware

--- common/src/middlewares/handlers/error-handler.ts
+++ common/src/middlewares/handlers/error-handler.ts
@@ -7,6 +7,8 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
+  console.error(err)
+
   res.status(400).send({
     errors: [{ message: 'something went wrong' }]
   })
