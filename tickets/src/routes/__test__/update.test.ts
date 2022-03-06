f7eb5e3f7c8d0acffa14f147473542e1f516919d
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-03-06 11:44:29 -0300

    test: ensure 404 and 401 return if the id does not exist or user is not authenticated

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { signin } from '../../test/setup'
+import mongoose from 'mongoose'
+
+it('returns a 404 if the provided id does not exist', async () => {
+  const cookie = signin()
+  const id = new mongoose.Types.ObjectId().toHexString()
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'Some title',
+      price: 300
+    })
+    .expect(404)
+})
+
+it('returns a 401 if the user is not authenticated', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString()
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+      title: 'Some title',
+      price: 300
+    })
+    .expect(401)
+})
+
+it('returns a 401 if the user does not own the ticket', async () => {
+
+})
+
+it('returns a 400 if the user provides an invalid title or price', async () => {
+
+})
+
+it('updates the ticket provided valid inputs', async () => {
+
+})
